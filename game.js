4ead80ed9bd047eba51e857c8eded31f611abaa0
Superdoggy <superdoggy.pr0gr4mm3r@gmail.com>
2015-07-31 14:06:00 -0400

    Create game.js

---
+++ game.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById('game');
+var canvascontext = canvas.getContext && canvas.getContext('2d');
+if(!canvascontext) {
+    alert('Pls upgrade browser');
+}
+else {
+    startGame();
+}
+function startGame() {
+
+}
