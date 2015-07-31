289c9e14f53576f29db8f0a83535a537ba1ba921
Superdoggy <superdoggy.pr0gr4mm3r@gmail.com>
2015-07-31 17:59:40 -0400

    Update game.js

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ else {
 function startGame() {
     var img = new Image();
     img.onload = function(){
-        canvascontext.drawImage(img, 0, 0, 37, 43, 100, 100, 37, 43);
+        canvascontext.drawImage(img, 0, 0, 37, 42, 100, 100, 37, 42);
     }
     img.src = 'https://cloud.githubusercontent.com/assets/13082525/9018290/9c490a28-37ab-11e5-8c6f-71568956a3c3.png';
 }
