3494ec19a0485a7400850cde50ea9a0665936da1
Superdoggy <superdoggy.pr0gr4mm3r@gmail.com>
2015-07-31 17:06:31 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+var pass = prompt('password?');
+while(prompt != 'meep') { // I really don't care if this password is blatantly obvious but I'd like to have a little privacy whilst developing. :P
+    pass = prompt('password?')   
+}
+}
 var canvas = document.getElementById('game');
 var canvascontext = canvas.getContext && canvas.getContext('2d');
 if(!canvascontext) {
