5b34e43697e9346574cda7c4e21c15e4bdd7304b
Superdoggy <superdoggy.pr0gr4mm3r@gmail.com>
2015-07-31 22:04:41 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -7,13 +7,13 @@ var Sprites = new function() {
     this.image.src = 'https://cloud.githubusercontent.com/assets/13082525/9018290/9c490a28-37ab-11e5-8c6f-71568956a3c3.png';
   };
   this.draw = function(canvascontext, spriteID, xpos, ypos, frameN) {
-    var spriteInfo = this.map[spriteID];
-    if (!spriteInfo) {
+    var s = this.map[spriteID];
+    if (!s) {
       window.alert('uhoh');
     }
     if (!frameN) {
      frameN = 0; 
     }
-    canvascontext.drawImage(this.image, spriteInfo.sx + frameN * spriteInfo.w, spriteInfo.sy, spriteInfo.w, spriteInfo.h, xpos, ypos, spriteInfo.w, spriteInfo.h);
+    canvascontext.drawImage(this.image, s.sx + frameN * s.w, s.sy, s.w, s.h, xpos, ypos, s.w, s.h);
   };
 }
