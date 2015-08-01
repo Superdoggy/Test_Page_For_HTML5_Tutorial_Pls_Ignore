c5fb964ac96da5e213a4237f584314091c596a29
Superdoggy <superdoggy.pr0gr4mm3r@gmail.com>
2015-07-31 21:46:02 -0400

    Update game.js

--- game.js
+++ game.js
@@ -8,12 +8,17 @@ if(!canvascontext) {
     alert('Pls upgrade browser');
 }
 else {
-    startGame();
+    Sprites.load({
+        player: {sx: 0, sy: 0, w: 37, h: 42}
+    }, startGame());
 }
 function startGame() {
-    var img = new Image();
-    img.onload = function(){
-        canvascontext.drawImage(img, 0, 0, 37, 42, 100, 100, 37, 42);
-    }
-    img.src = 'https://cloud.githubusercontent.com/assets/13082525/9018290/9c490a28-37ab-11e5-8c6f-71568956a3c3.png';
+    
+    Sprites.draw(canvascontext, "player", 0, 0);
+    
+    //var img = new Image();
+    //img.onload = function(){
+    //    canvascontext.drawImage(img, 0, 0, 37, 42, 100, 100, 37, 42);
+    //}
+    //img.src = 'https://cloud.githubusercontent.com/assets/13082525/9018290/9c490a28-37ab-11e5-8c6f-71568956a3c3.png';
 }
