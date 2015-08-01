0c40a19f91c66d3669a1eea6b63deb964b2ff4ab
Superdoggy <superdoggy.pr0gr4mm3r@gmail.com>
2015-07-31 21:39:08 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,1 +1,16 @@
-
+var Sprites = new function() {
+  this.map = {};
+  this.load = function(spriteData, callback) {
+    this.map = spriteData;
+    this.image = new Image();
+    this.image.onload = callback;
+    this.image.src = 'https://cloud.githubusercontent.com/assets/13082525/9018290/9c490a28-37ab-11e5-8c6f-71568956a3c3.png';
+  };
+  this.draw = function(canvascontext, spriteID, xpos, ypos, frameN) {
+    var spriteInfo = this.map[spriteID];
+    if (!frameN) {
+     frameN = 0; 
+    }
+    canvascontext.drawImage(this.image, spriteInfo.sx + frameN * spriteInfo.w, spriteInfo.sy, spriteInfo.w, spriteInfo.h, xpos, ypos, spriteInfo.w, spriteInfo.h);
+  };
+}
