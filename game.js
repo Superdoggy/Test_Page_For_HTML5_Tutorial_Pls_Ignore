fba408b992a2058cc4ec0c93801dac9ecd8b953f
Superdoggy <superdoggy.pr0gr4mm3r@gmail.com>
2015-08-01 22:37:35 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,14 +1,11 @@
 var pass = prompt('password?', '???');
 while(pass != 'meep') { // I really don't care if this password is blatantly obvious but I'd like to have a little privacy whilst developing. :P
-    pass = prompt('password?', '???');
+  pass = prompt('password?', '???');
 }
-var canvas = document.getElementById('game');
-var canvascontext = canvas.getContext && canvas.getContext('2d');
-if(!canvascontext) {
-    alert('Pls upgrade browser');
+var sprites = {
+  player: {sx: 0, sy: 0, w: 37, h: 42}
+};
+var gameStart = function() {
+  Sprites.load(sprites, function() {Sprites.draw(canvascontext, "player", 0, 0);});   
 }
-else {
-    Sprites.load({
-        player: {sx: 0, sy: 0, w: 37, h: 42}
-    }, function() {Sprites.draw(canvascontext, "player", 0, 0);});
-}
+Game.initialize(sprites, gameStart);
