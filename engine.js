c525890b74fcea99b3eb3d8ef17e15838fa54524
Superdoggy <superdoggy.pr0gr4mm3r@gmail.com>
2015-08-01 22:30:38 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,3 +1,25 @@
+var Game = new function() {
+  this.initialize = function(spritedata, callback) {
+    this.canvas = document.getElementByID('game');
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    
+    this.canvascontext = this.canvas.getContext && canvas.getContext('2d');
+    if(!canvascontext) {
+      return alert('ye useless browser cannot render canvas');
+    }
+    this.setupInput();
+    this.loop();
+    Sprites.load(spritedata, callback);
+  }
+  this.setupInput = function() {
+    
+  }
+  this.loop = function() {
+    
+  }
+}
+
 var Sprites = new function() {
   this.map = { };
   this.load = function(spriteData, callback) {
