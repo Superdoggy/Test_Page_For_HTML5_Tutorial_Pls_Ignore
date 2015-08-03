45ee4c6a5a00eb024c51fa2e3d15723ebc8c8cc8
Superdoggy <superdoggy.pr0gr4mm3r@gmail.com>
2015-08-02 21:25:58 -0400

    Update game.js

--- game.js
+++ game.js
@@ -6,6 +6,6 @@ var sprites = {
   player: {sx: 0, sy: 0, w: 37, h: 42}
 };
 var gameStart = function() {
-  Sprites.load(sprites, function() {Sprites.draw(canvascontext, "player", 0, 0);});   
+  Sprites.load(sprites, function() {Sprites.draw("player", 0, 0);});   
 }
 Game.initialize(sprites, gameStart);
