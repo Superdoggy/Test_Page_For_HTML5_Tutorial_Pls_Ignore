22278f78ca5be2617f8dba1e60a4765a33cdcb97
Superdoggy <superdoggy.pr0gr4mm3r@gmail.com>
2015-08-04 18:20:34 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -93,7 +93,7 @@ var Game = new function() {
     boards[i] = sprite;
   };
 }
-var Sprites = function() {
+var Sprites = new function() {
   this.map = { };
   this.load = function(spriteData, callback) {
     this.map = spriteData;
@@ -102,7 +102,7 @@ var Sprites = function() {
     this.image.src = 'https://cloud.githubusercontent.com/assets/13082525/9018290/9c490a28-37ab-11e5-8c6f-71568956a3c3.png';
   };
   this.draw = function(spriteID, xpos, ypos, frameN) {
-    var s = this.map[spriteID];
+    var s = Sprites.map[spriteID];
     if (!frameN) {
      frameN = 0; 
     }
