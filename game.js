418a73c10d513169ec6efc81c4af14a6c55aab73
Superdoggy <superdoggy.pr0gr4mm3r@gmail.com>
2015-08-15 23:46:03 -0400

    Update game.js

--- game.js
+++ game.js
@@ -79,6 +79,23 @@ playermissile.prototype.draw = function(canvascontext) {
   Sprites.draw('missile', this.x, this.y);
 }
 playermissile.prototype.type = Playerprojectiletype;
+var enemy = function(blueprint, override) {
+  var base = {A: 0, B: 0, C: 0, D: 0, E: 0, F: 0, G: 0, H: 0};
+  for (var property in base) {
+    this[property] = base[property];
+  }
+  for (var property in blueprint) {
+    this[property] = blueprint[property];
+  }
+  if(override) {
+    for (var property in override) {
+      this[property] = override[property];
+    }
+  }
+  this.w = Sprites.map[this.sprite].w;
+  this.h = Sprites.map[this.sprite].h;
+  this.time = 0;
+}
 
 
 
